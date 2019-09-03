f8643a83a2bdd7e79fcb86f67bf2667f9ddca368
ash1969 <nikhil.sonksbs@gmail.com>
2019-09-04 02:21:09 +0530

    Backend Synced

--- application/src/containers/BurgerBuilder/BurgerBuilder.js
+++ application/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -58,7 +59,25 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert("Dhanyavaad! Thank You!");
+        //alert("Dhanyavaad! Thank You!");
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Abeer Singh',
+                address: {
+                    street: 'Ashiyana Central',
+                    zipCode: '99554',
+                    country: 'India'
+                },
+                email: 'helloabeer@test.com',
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
+
     }
 
     removeIngredientHandler = (type) => {
