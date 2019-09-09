d77209fb98554eb745b95719027fb608b5d84c33
ash1969 <nikhil.sonksbs@gmail.com>
2019-09-10 04:25:48 +0530

    Routing Improved

--- application/src/components/Navigation/NavigationItems/NavigationItems.js
+++ application/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -4,8 +4,8 @@ import NavigationItem from './NavigationItem/NavigationItem'
 
 const navigationItems = () => 
    <ul className={classes.NavigationItems}>
-        <NavigationItem link="/" active>HungerHook</NavigationItem>
-        <NavigationItem link="/">Checkout</NavigationItem>
+        <NavigationItem link="/">HungerHook</NavigationItem>
+        <NavigationItem link="/orders">Orders</NavigationItem>
    </ul>
 
 
