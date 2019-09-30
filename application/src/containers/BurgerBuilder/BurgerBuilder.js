7a80689a90a7872a7b2cba7034208f134112b4aa
ash1969 <nikhil.sonksbs@gmail.com>
2019-10-01 03:46:19 +0530

    UI Through Redux

--- application/src/containers/BurgerBuilder/BurgerBuilder.js
+++ application/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,6 @@ import * as actionTypes from '../../store/actions'
 
 class BurgerBuilder extends Component {
     state = {
-       purchasable: false, 
        purchasing: false,
        loading: false,
        error: false
@@ -38,7 +37,7 @@ class BurgerBuilder extends Component {
         .reduce((sum, el) => {
             return sum + el;
         },  0);
-        this.setState({purchasable: sum > 0});
+        return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -90,7 +89,7 @@ class BurgerBuilder extends Component {
               disabled={disabledInfo}
               price={this.props.price}
               ordered={this.purchaseHandler}
-              purchasable={this.state.purchasable} />
+              purchasable={this.updatePurchaseState(this.props.ings)} />
             </Aux>;
             orderSummary = 
             <OrderSummary ingredients={this.props.ings} purchaseCancelled={this.purchaseCancelHandler}
