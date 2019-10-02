f4f5aa91ad44da37432b56117f8589dd748b5cf3
ash1969 <nikhil.sonksbs@gmail.com>
2019-10-03 03:26:04 +0530

    Fetching Orders Through Redux

--- application/src/store/actions/order.js
+++ application/src/store/actions/order.js
@@ -1,5 +1,6 @@
 import * as actionTypes from './actionTypes'
 import axios from '../../axios-orders'
+import { fetchIngredientsFailed } from './burgerBuilder';
 
 export const purchaseBurgerSuccess = (id, orderData) => {
     return {
@@ -39,4 +40,46 @@ export const purchaseInit = () => {
     return {
         type: actionTypes.PURCHASE_INIT,
     };
+};
+
+export const fetchOrdersSuccess = (orders) => {
+     return {
+        type: actionTypes.FETCH_ORDERS_SUCCESS,
+        orders: orders,
+     };
+};
+
+export const fetchOrdersFail = (error) => {
+    return {
+       type: actionTypes.FETCH_ORDERS_FAIL,
+       error: error,
+    };
+};
+
+export const fetchOrdersStart = () => {
+    return {
+       type: actionTypes.FETCH_ORDERS_START,
+       
+    };
+};
+
+export const fetchOrders = () => {
+    return dispatch => {
+        dispatch(fetchOrdersStart());
+        axios.get('/orders.json')
+        .then( res => {
+            const fetchedOrders = [];
+            for(let key in res.data)
+            {
+               fetchedOrders.push({
+                  ...res.data[key],
+                  id: key,
+               });
+            }
+            dispatch(fetchOrdersSuccess(fetchedOrders));
+        })
+        .catch( err => {
+            dispatch(fetchOrdersFail(err));
+        });
+    }
 };
