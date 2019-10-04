2f8b897cf92e6855abffc6b2ce90213fc112c557
ash1969 <nikhil.sonksbs@gmail.com>
2019-10-04 22:58:20 +0530

    Utility Function Made Shared

--- application/src/containers/Checkout/ContactData/ContactData.js
+++ application/src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import Input from '../../../components/UI/Input/Input'
 import { connect } from 'react-redux'
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler'
 import * as actions from '../../../store/actions/index'
+import { updateObject } from '../../../shared/utility'
 
 class ContactData extends  Component {
     state = {
@@ -136,15 +137,16 @@ class ContactData extends  Component {
     }
 
     inputChangedHandler = (event, inputIdentifier) => {
-       const updatedOrderForm = {
-           ...this.state.orderForm
-       }
-       const updatedFormElement = {
-           ...updatedOrderForm[inputIdentifier]
-       };
-       updatedFormElement.value = event.target.value;
-       updatedFormElement.touched = true;
-       updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation)
+       const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+        value: event.target.value,
+        touched: true,
+        valid: this.checkValidity(event.target.value, this.state.orderForm[inputIdentifier].validation)
+       });
+
+       const updatedOrderForm = updateObject(this.state.orderForm, {
+        [inputIdentifier]: updatedFormElement,
+       });
+
        updatedOrderForm[inputIdentifier] = updatedFormElement;
 
        let formIsValid = true;
